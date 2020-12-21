0a815dd42dc0d7bba75c1963f88afe099c9cbe27
aundus <aundus@stevens.edu>
2020-12-21 17:30:38 -0500

    create bookshelf view

--- bookshelf_management/apps/mgmt/views.py
+++ bookshelf_management/apps/mgmt/views.py
@@ -1,8 +1,27 @@
 from .models import Book, Bookshelf, BookshelfToBook
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+from django.views import generic
+
+# class BookListView(generic.ListView):
+#     model = Book
+
+    
+
 
 
 def booksInBookshelf(request, bookshelfId):
+    bookshelfName = Bookshelf.objects.get(id=bookshelfId).bookshelf
     idList = BookshelfToBook.objects.filter(fk_bookshelves=bookshelfId).values_list('fk_books', flat=True) 
     books = Book.objects.filter(id__in=idList)
-    return HttpResponse(books)
+
+    context = {
+        'books': books,
+        'total': len(books),
+        'bookshelf': bookshelfName,
+    }
+
+
+    return render(request, 'index.html', context=context)
